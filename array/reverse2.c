813068f17c3318a58a6861a6c1208e6d00e8886c
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-01-27 06:03:23 +0800

    Gavin:Array

---
+++ array/reverse2.c
@@ -0,0 +1,26 @@
+/**
+ * @Author ZhangGJ
+ * @Date 2021/01/26 22:03
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int i, n;
+
+    printf("How many numbers do you want to reverse? ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter %d numbers: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("In reverse order: ");
+    for (i = n - 1; i >= 0; i--) {
+        printf(" %d", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
